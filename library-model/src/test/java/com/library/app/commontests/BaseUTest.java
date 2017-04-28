d5a7529d0bf9e0ad6fa02ff713fabb9296e8d4aa
Bruno Bitencourt Luiz <brunowdev@gmail.com>
2017-04-28 19:51:37 -0300

    Criada classe para lidar com constantes de persistência

--- library-model/src/test/java/com/library/app/commontests/BaseUTest.java
+++ library-model/src/test/java/com/library/app/commontests/BaseUTest.java
@@ -1,35 +1,34 @@
 package com.library.app.commontests;
 
-import java.util.Optional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
+import com.library.app.common.constants.PersistenceConstants;
+import com.library.app.commontests.util.DBCommandTransactionalExecutor;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 
-import com.library.app.commontests.util.DBCommandTransactionalExecutor;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Optional;
 
 @Ignore
 public class BaseUTest {
 
-	protected static EntityManagerFactory emf;
-	protected static EntityManager em;
-	protected static DBCommandTransactionalExecutor dbCommandTransactionalExecutor;
+    protected static EntityManagerFactory emf;
+    protected static EntityManager em;
+    protected static DBCommandTransactionalExecutor dbCommandTransactionalExecutor;
 
-	@BeforeClass
-	public static void initTestCase() {
-		emf = Persistence.createEntityManagerFactory("libraryPU");
-		em = emf.createEntityManager();
-		dbCommandTransactionalExecutor = new DBCommandTransactionalExecutor(em);
-	}
+    @BeforeClass
+    public static void initTestCase() {
+        emf = Persistence.createEntityManagerFactory(PersistenceConstants.PERSISTENCE_UNIT_NAME);
+        em = emf.createEntityManager();
+        dbCommandTransactionalExecutor = new DBCommandTransactionalExecutor(em);
+    }
 
-	@AfterClass
-	public static void closeEntityManager() {
-		Optional.of(em).get().close();
-		Optional.of(emf).get().close();
-	}
+    @AfterClass
+    public static void closeEntityManager() {
+        Optional.of(em).get().close();
+        Optional.of(emf).get().close();
+    }
 
 }
